f569e825b9acba77b67704ec2edc9d4e6316c3e0
brianstatesthat <mbspeelman@gmail.com>
2025-09-04 17:04:18 +0200

    rock with me now!

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function MainContent() {
   return (
-    <main style={{ backgroundColor: 'navy', color: 'white' }}>
-        <p style={{ color: 'white', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+    <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   )
 }
