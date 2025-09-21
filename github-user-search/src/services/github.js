79bf48043bf12cd99cdca75edafeb7767ec73616
brianstatesthat <mbspeelman@gmail.com>
2025-09-21 12:23:00 +0200

    removed unnecessary github js service file

--- github-user-search/src/services/github.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const searchUsers = async (username) => {
-  const response = await axios.get(`https://api.github.com/search/users?q=${username}`);
-  return response.data;
-};
