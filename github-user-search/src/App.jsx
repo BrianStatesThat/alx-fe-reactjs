52cdba80d9f985c60dec6e7c4daa6f91c44ba7f7
brianstatesthat <mbspeelman@gmail.com>
2025-09-21 12:13:40 +0200

    installed react router dom and ammended the app jsx file accordingly

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,23 @@
-import UserSearch from './components/UserSearch'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserSearch from './components/UserSearch';
+import './App.css';
+
+function Profile() {
+  return <div>Profile Page (Coming Soon)</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
-      <UserSearch />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <h1>GitHub User Search</h1>
+        <Routes>
+          <Route path="/" element={<UserSearch />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
