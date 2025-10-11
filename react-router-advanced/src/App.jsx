54a5920f3d7a4a11c4e380e7d3f2e302b58479fd
brianstatesthat <mbspeelman@gmail.com>
2025-10-11 19:43:24 +0200

    editted app jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import BlogPost from './pages/BlogPost';
 import NotFound from './pages/NotFound';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog/:postId" element={<BlogPost />} />
-      <Route path="/profile/*" element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
