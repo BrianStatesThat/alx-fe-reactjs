03080a3f3eb6f692659c5e9feb026466aa4da23b
brianstatesthat <mbspeelman@gmail.com>
2025-10-11 20:30:58 +0200

    new components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,14 +3,22 @@ import Home from './components/Home';
 import BlogPost from './components/BlogPost';
 import Profile from './components/Profile';
 import NotFound from './components/NotFound';
+import ProtectedRoute from './components/ProtectedRoute'; // Required import
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog/:id" element={<BlogPost />} /> {/* ✅ Required dynamic route */}
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute> {/* Required usage */}
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
